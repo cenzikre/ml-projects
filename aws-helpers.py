ba2b49fc887a2e9af4c1f0fc484d2cb4680f883f
cenzikre <140105159+cenzikre@users.noreply.github.com>
2024-11-16 18:35:09 -0500

    Update aws-helpers.py

--- aws-helpers.py
+++ aws-helpers.py
@@ -3,6 +3,7 @@ import pickle
 import io
 import os
 
+# Interact with s3 bucket
 
 def save_pkl_to_s3(obj, file_name, bucket_name=None, folder_name=None):
     pkl_object = pickle.dumps(obj)
@@ -16,3 +17,20 @@ def load_pkl_from_s3(file_name, bucket_name=None, folder_name=None):
     response = s3.get_object(Bucket=bucket_name, Key=object_key)
     serialized_data = response['Body'].read()
     return pickle.loads(serialized_data)
+
+def save_tensor_to_s3(tensor, file_name, bucket_name=None, folder_name=None):
+    import torch
+    buffer = io.BytesIO()
+    torch.save(tensor, buffer)
+    buffer.seek(0)
+    s3 = boto3.client('s3')
+    object_key = os.path.join(folder_name, file_name + '.pt')
+    s3.put_object(Bucket=bucket_name, Key=object_key, Body=buffer.getvalue())
+
+def load_tensor_from_s3(file_name, bucket_name=None, folder_name=None):
+    import torch
+    s3 = boto3.client('s3')
+    object_key = os.path.join(folder_name, file_name + '.pt')
+    response = s3.get_object(Bucket=bucket_name, Key=object_key)
+    buffer = io.BytesIO(response['Body'].read())
+    return torch.load(buffer)
