5b48504c9970c65cb9a449845a7f47d00d2c8ed3
cenzikre <140105159+cenzikre@users.noreply.github.com>
2024-11-16 18:26:29 -0500

    Update and rename aws-helpers to aws-helpers.py

--- aws-helpers.py
+++ aws-helpers.py
@@ -9,3 +9,10 @@ def save_pkl_to_s3(obj, file_name, bucket_name=None, folder_name=None):
     s3 = boto3.client('s3')
     object_key = os.path.join(folder_name, file_name + '.pkl')
     s3.put_object(Bucket=bucket_name, Key=object_key, Body=pkl_object)
+
+def load_pkl_from_s3(file_name, bucket_name=None, folder_name=None):
+    s3 = boto3.client('s3')
+    object_key = os.path.join(folder_name, file_name + '.pkl')
+    response = s3.get_object(Bucket=bucket_name, Key=object_key)
+    serialized_data = response['Body'].read()
+    return pickle.loads(serialized_data)
